6828676ab4673c3432b45bb9b3a678b64eb4f76a
Slim80 <bestboss1980@gmail.com>
2016-06-12 06:16:04 +0200

    cpufreq: ad Electroactive CPU gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -472,6 +472,9 @@ extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
 extern struct cpufreq_governor cpufreq_gov_despair;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_despair)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTROACTIVE)
+extern struct cpufreq_governor cpufreq_gov_electroactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electroactive)
 #endif
 
 /*********************************************************************
